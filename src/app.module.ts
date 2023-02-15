d8776256110e4c6a30db000e9200acf83a8fdab1
uasosa <usosa@emergencias.com.ar>
2023-02-15 13:00:36 -0300

    inicio mongodb y mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, '../public')
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule
+  ],
   controllers: [],
   providers: [],
 })
